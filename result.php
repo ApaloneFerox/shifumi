0c7be86e019086e8aac05e59e57ee27e157f3812
mael <maelboutemy@hotmail.fr>
2019-01-24 14:44:02 +0100

    HTML/CSS

--- result.php
+++ result.php
@@ -2,6 +2,7 @@
     /*
         SHIFUMI RESULTAT
     */
+    var_dump($_POST);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,9 +10,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SHIFUMI : DO YOU WIN ?</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe" rel="stylesheet">
+    <title>SHI-FU-MI : DO YOU WIN ?</title>
 </head>
 <body>
-    
+
+   <h1>Shi-fu-mi</h1>
+
+   <div class="result">
+       <img src="" alt="Image joueur">
+       <p>Tu pue le chat </p>
+       <img src="" alt="Image ordinateur">
+   </div>
+
+    <div class="buttonAgain">
+    <label for="again">Try again ! </label>
+    <a href="index.php"><input type="button" name="again" value="もう一度やり直してください"></a>
+    </div>
+
 </body>
 </html>
